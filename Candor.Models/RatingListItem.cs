09862ab407700b53c685c83ab74055bfae0b59e3
onedayherenow@gmail.com <onedayherenow@gmail.com>
2022-04-06 22:17:41 -0400

    Display name

--- Candor.Models/RatingListItem.cs
+++ Candor.Models/RatingListItem.cs
@@ -10,6 +10,7 @@ namespace Candor.Models
 	public class RatingListItem
 	{
 		public string UserName { get; set; }
+		[Display(Name = "Idea Id")]
 		public int IdeaId { get; set; }
 		[Display(Name = "Rating ID")]
 		public int RatingId { get; set; }
